8ef1b1fef70d0540149a2ff19e18c87d236b3ca5
sadhin28 <taosifsadhin113@gmail.com>
2025-06-05 17:57:39 +0600

    add pushon hover

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const LatestNews = () => {
     return (
         <div className="gap-2 flex items-center p-4 bg-[#F3F3F3]">
-            <p className="w-[110px] h-[48px] flex text-xl text-white justify-center items-center bg-[#D72050]">Latest</p>
-            <Marquee className="space-x-10">
+            <p className="w-[100px] h-[40px] flex text-xl text-white justify-center items-center bg-[#D72050]">Latest</p>
+            <Marquee pauseOnHover='true'className="space-x-10">
                 <Link to='/news'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, minima.</Link>
                 <Link to='/news'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, minima.</Link>
                 <Link to='/news'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, minima.</Link>
