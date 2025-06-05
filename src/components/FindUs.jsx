f307904d6d77456affe1ff74ef3a1c1cff7f26e6
sadhin28 <taosifsadhin113@gmail.com>
2025-06-05 23:36:48 +0600

    add find us on section

---
+++ src/components/FindUs.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const FindUs = () => {
+    return (
+          <div>
+            <h1 className="font-bold mb-3 mt-5">Find Us On</h1>
+
+            <div className="join join-vertical *:bg-base-100 flex">
+                <button className="btn join-item justify-start">Button</button>
+                <button className="btn join-item justify-start">Button</button>
+                <button className="btn join-item justify-start">Button</button>
+            </div>
+        </div>
+    );
+};
+
+export default FindUs;
