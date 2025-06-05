cc3304f4d4961b6182427226bbb03198be1af82d
sadhin28 <taosifsadhin113@gmail.com>
2025-06-06 01:26:09 +0600

    add news card component

--- src/components/Page/CategoryNews.jsx
+++ src/components/Page/CategoryNews.jsx
@@ -1,11 +1,18 @@
 import { useLoaderData } from "react-router-dom";
+import NewsCard from "../layout-component/NewsCard";
 
 const CategoryNews = () => {
-    const {data} = useLoaderData()
-    console.log(data)
+    const {data:news} = useLoaderData()
+    console.log(news)
     return (
         <div>
-            <h1 className="font-bold">({data.length}) News Found In This Category</h1>
+            <h1 className="font-bold">Dragon News Home</h1>
+            <h1 className="font-bold text-xs">({news.length}) News Found InThis Category</h1>
+            <div>
+                {
+                    news.map(singleNews=><NewsCard key={singleNews.category_id} singleNews={singleNews}></NewsCard>)
+                }
+            </div>
         </div>
     );
 };
