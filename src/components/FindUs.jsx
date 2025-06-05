42f2178a3d7bc432a0ded7184cd61e9ba18a51f7
sadhin28 <taosifsadhin113@gmail.com>
2025-06-05 23:40:38 +0600

    add facebook ,instragram icom

--- src/components/FindUs.jsx
+++ src/components/FindUs.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 const FindUs = () => {
     return (
@@ -6,9 +7,9 @@ const FindUs = () => {
             <h1 className="font-bold mb-3 mt-5">Find Us On</h1>
 
             <div className="join join-vertical *:bg-base-100 flex">
-                <button className="btn join-item justify-start">Button</button>
-                <button className="btn join-item justify-start">Button</button>
-                <button className="btn join-item justify-start">Button</button>
+                <button className="btn join-item justify-start text-xl"><FaFacebook></FaFacebook> Facebook</button>
+                <button className="btn join-item justify-start text-xl"><FaInstagram></FaInstagram> Instragram</button>
+                <button className="btn join-item justify-start text-xl"><FaTwitter></FaTwitter> Twitter</button>
             </div>
         </div>
     );
