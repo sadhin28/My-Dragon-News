ace94e8a7098d7db337ccd2ef7fa6b334e685aae
sadhin28 <taosifsadhin113@gmail.com>
2025-06-05 17:22:09 +0600

    implement time and date

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
 
+import moment from 'moment';
+import logo from '../assets/logo.png'
 const Header = () => {
     return (
-        <div>
-           <h1>header</h1>
+        <div className='flex flex-col justify-center items-center'>
+           <div>
+              <img className='py-3 w-[300px]' src={logo} alt="" />
+           </div>
+           <p className=' text-gray-400'>Journalism Without Fear or Favour</p>
+            <p>{moment().format("ddd, MMMM Do YYYY")}</p>
         </div>
     );
 };
