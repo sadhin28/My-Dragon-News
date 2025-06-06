b619b8ee5d69040831e14261289bfd793dc45d2a
sadhin28 <taosifsadhin113@gmail.com>
2025-06-06 23:09:02 +0600

    create a context api

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+export const AuthContext=createContext();
+const AuthProvider = ({children}) => {
+    const [user,setuser]=useState(
+        {
+            name:'Taosif'
+        }
+    )
+
+    const authInfo={
+        user,
+        setuser
+    }
+    return (
+       <AuthContext.Provider value={authInfo}>
+            {children}
+       </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
