e01c532079717a0e3b722fcc8face8894845cbb7
sadhin28 <taosifsadhin113@gmail.com>
2025-06-06 20:35:02 +0600

    add login form

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const AuthLayout = () => {
     return (
-        <div>
-            AuthLayout
+        <div className="header bg-[#F3F3F3] ">
+           <div className="w-11/12 mx-auto">
+                  <header >
+             <Navbar></Navbar>
+           </header>
             <Outlet></Outlet>
+           </div>
+         
         </div>
     );
 };
