5b1103ff0d8ccc04b84121b408c660831e8c386c
sadhin28 <taosifsadhin113@gmail.com>
2025-06-07 01:15:58 +0600

    implement register

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import { Link, NavLink } from "react-router-dom";
 import userIcon from '../assets/user.png'
 import { AuthContext } from "../provider/AuthProvider";
-import {useContext } from "react";
+import {use, useContext } from "react";
 const Navbar = () => {
     const {user,Logout}=useContext(AuthContext)
     console.log(user)
     return (
         <div className="flex justify-between items-center">
            <div>
-            
+              <p>{user && user.displayName}</p>
            </div>
             <div className="nav space-x-5">
                 <NavLink to='/'>Home</NavLink>
