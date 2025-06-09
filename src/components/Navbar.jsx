ca0d0ef4006909a8864a4003ba69c6b3f56b56ae
sadhin28 <taosifsadhin113@gmail.com>
2025-06-09 14:13:36 +0600

    add loging page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     const { user, Logout } = useContext(AuthContext)
    
     return (
-        <div className="flex justify-between items-center">
+        <div className="flex md:mb-0 mb-5 justify-between items-center text-xs md:text-xl">
             <div>
                 <p>{user && user.displayName}</p>
             </div>
@@ -17,12 +17,12 @@ const Navbar = () => {
             </div>
             <div className="flex gap-2 items-center">
                 {
-                    user ? <img className="w-10 h-10 rounded-full object-cover" src={user && user.
+                    user ? <img className="md:w-10 md:h-10 w-5 h-5 rounded-full object-cover" src={user && user.
                         photoURL} alt="" />:<img className="w-10 h-10 rounded-full object-cover" 
                         src={userLogo} alt="" />
                 }
                 {
-                    user && user?.email ? <Link to="/" onClick={Logout} className="btn btn-neutral rounded-none">Log-Out</Link> : <Link to='auth/login' className="btn btn-neutral rounded-none">Log-In</Link>
+                    user && user?.email ? <Link to="/" onClick={Logout} className="btn btn-neutral rounded-none">Log-Out</Link> : <Link to='auth/login' className="btn btn-neutral text-xs md:text-xl rounded-none">Log-In</Link>
                 }
             </div>
         </div>
