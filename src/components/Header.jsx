329c348c7526f47c7fba17e7a22a8e4dfd62f15d
sadhin28 <taosifsadhin113@gmail.com>
2025-07-01 19:57:28 +0600

    add flag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,21 @@
 
 import moment from 'moment';
-import logo from '../assets/logo.png'
+import logo from '../assets/satradal_img.png'
 const Header = () => {
     return (
-        <div className='header flex flex-col justify-center items-center'>
-           <div>
-              <img className='py-3 w-[300px]' src={logo} alt="" />
-           </div>
-           <p className=' text-gray-400 font-Poppins'>Journalism Without Fear or Favour</p>
-            <p>{moment().format("ddd, MMMM Do YYYY")}</p>
+        <div className='header '>
+            <div className='flex justify-between'>
+                <div><img className='py-3 h-[100px] w-full' src={logo} alt="" /></div>
+                <div><img className='py-3 h-[100px] w-full' src={logo} alt="" /></div>
+                <div><img className='py-3 h-[100px] w-full' src={logo} alt="" /></div>
+
+            </div>
+            <div>
+                <p className=' text-gray-400 font-Poppins'>বাংলাদেশ জাতীয়তাবাদী ছাত্রদল</p>
+                <p className=' text-gray-400 font-Poppins'>সিটি বিশ্ববিদ্যালয়</p>
+                <p className=' text-gray-400 font-Poppins'>শিক্ষা ঐক্য প্রগতি ছাত্রদলের মূল নীতি</p>
+                <p>{moment().format("ddd, MMMM Do YYYY")}</p>
+            </div>
         </div>
     );
 };
