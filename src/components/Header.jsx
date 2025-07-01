b9c214eed52db537501a295585e8da8915c95356
sadhin28 <taosifsadhin113@gmail.com>
2025-07-01 20:03:10 +0600

    add sadradol flag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,16 @@ import logo from '../assets/satradal_img.png'
 const Header = () => {
     return (
         <div className='header '>
-            <div className='flex justify-between'>
+            <div className='flex   mx-10  justify-between'>
                 <div><img className='py-3 h-[100px] w-full' src={logo} alt="" /></div>
                 <div><img className='py-3 h-[100px] w-full' src={logo} alt="" /></div>
                 <div><img className='py-3 h-[100px] w-full' src={logo} alt="" /></div>
 
             </div>
-            <div>
-                <p className=' text-gray-400 font-Poppins'>বাংলাদেশ জাতীয়তাবাদী ছাত্রদল</p>
-                <p className=' text-gray-400 font-Poppins'>সিটি বিশ্ববিদ্যালয়</p>
-                <p className=' text-gray-400 font-Poppins'>শিক্ষা ঐক্য প্রগতি ছাত্রদলের মূল নীতি</p>
+            <div className='text-center '>
+                <p className=' text-gray-400 font-Poppins'>বাংলাদেশ জাতীয়তাবাদী ছাত্রদল ।</p>
+                <p className=' text-gray-400 font-Poppins'>সিটি বিশ্ববিদ্যালয়।</p>
+                <p className=' text-gray-400 font-Poppins'>শিক্ষা ঐক্য প্রগতি ছাত্রদলের মূল নীতি ।</p>
                 <p>{moment().format("ddd, MMMM Do YYYY")}</p>
             </div>
         </div>
