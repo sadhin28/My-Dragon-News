a2a6e194b7a2112665854c581db177ab582cb10c
sadhin28 <taosifsadhin113@gmail.com>
2025-07-01 20:15:13 +0600

    add logo flag and family image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,19 @@
 
 import moment from 'moment';
-import logo from '../assets/satradal_img.png'
+// import flag from '../assets/satradal_img.png'
+import logo from '../assets/satradolLogo_files/জাতীয়তাবাদী_ছাত্রদলের_লোগো(1).svg.png'
+import flag from '../assets/satradolLogo_files/Flag_of_Bangladesh_Jatiotabadi_Chatra_Dal.svg.png'
+import flmilyimg from '../assets/satradolLogo_files/thumbnail-of-bnp-news-25--e489.jpg'
 const Header = () => {
     return (
         <div className='header '>
-            <div className='flex   mx-10  justify-between'>
-                <div><img className='py-3 h-[100px] w-full' src={logo} alt="" /></div>
-                <div><img className='py-3 h-[100px] w-full' src={logo} alt="" /></div>
-                <div><img className='py-3 h-[100px] w-full' src={logo} alt="" /></div>
+            <div className='flex w-11/12 mx-auto  justify-between'>
+                <div><img className='py-3 h-[100px] w-[100px]' src={logo} alt="" /></div>
+                <div><img className='py-3 h-[100px] w-[120px]' src={flag} alt="" /></div>
+                <div><img className='py-3 h-[100px] w-[100px]' src={flmilyimg} alt="" /></div>
 
             </div>
-            <div className='text-center '>
+            <div className='text-center mx-10'>
                 <p className=' text-gray-400 font-Poppins'>বাংলাদেশ জাতীয়তাবাদী ছাত্রদল ।</p>
                 <p className=' text-gray-400 font-Poppins'>সিটি বিশ্ববিদ্যালয়।</p>
                 <p className=' text-gray-400 font-Poppins'>শিক্ষা ঐক্য প্রগতি ছাত্রদলের মূল নীতি ।</p>
